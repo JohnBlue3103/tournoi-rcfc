39525ac59a37944d1c5ffbf072a5db4604aeec77
John Bernard <john.blue3103@gmail.com>
2026-06-10 16:06:53 +0200

    Config Supabase

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Remplace ces deux valeurs par celles de ton projet Supabase
 // Settings > API dans le dashboard Supabase
-const SUPABASE_URL  = 'https://XXXXXXXXXXXX.supabase.co';
-const SUPABASE_ANON = 'eyJhbGciOi...'; // anon public key
+const SUPABASE_URL  = 'https://zwyalfnyplkojumdvtvt.supabase.co';
+const SUPABASE_ANON = 'sb_publishable_jLfgEbdyuBmNn1yjwTcJYg_YaGjSPle';
 
 const sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON);
